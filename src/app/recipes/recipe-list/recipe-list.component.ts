688f43af35396d9befbd2cf6e1114f74c7947af9
Marina Melaré <melare.nina@gmail.com>
2020-04-03 16:21:02 -0300

    Adding content to the recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+//importing the model to be used as a type below
+import{ Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   
-  recipes = [];
+  //defining the type of the recipes to an Array of Recipe (the model created)
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a text', 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/healthy-eating-ingredients-1296x728-header.jpg?w=1155&h=1528')
+  ];
 
   constructor() { }
 
