b57549df3f023affa8dc571457f9150ffb555961
Marina Melaré <melare.nina@gmail.com>
2020-04-04 18:46:35 -0300

    Creating Ingredient Model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient{
+    //This will create the variables name and amount, and automatically assign the values recieved in the
+    //constructor to the variables
+    constructor(public name: string, public amount: number){}
+}
+
