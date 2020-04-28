468ef6e210781030fc116fd61c0c2985454c7260
Marina Melaré <melare.nina@gmail.com>
2020-04-28 20:57:53 -0300

    Adding Ingredients to Recipes

--- src/app/Services/recipe.service.ts
+++ src/app/Services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 
@@ -12,12 +13,18 @@ export class RecipeService{
             'Tasty Schnitzel',
             'A super tasty Schnitzel - just awesome!',
             'https://www.telegraph.co.uk/content/dam/food-and-drink/2019/06/05/Chicken-Schnitzel-PHOTOGRAPHY-Andrew-Twort-xlarge_trans_NvBQzQNjv4BqYVJfVX8lOXOA23ylB4y8VVVse9JsN00kzbUr3IXHaGo.jpg',
-            []),
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
         new Recipe(
             'Big Fat Burguer',
             'What else you need to say?',
             'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cf29c385555335.5d7fb3c164383.jpg',
-            [])
+            [
+                new Ingredient('Bread', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
 
       getRecipe(){
