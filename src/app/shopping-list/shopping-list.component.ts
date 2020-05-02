507427ec6ca4d79d136ada31b58fa4285f5367b7
Marina Melaré <melare.nina@gmail.com>
2020-05-02 13:39:07 -0300

    Fixing OnDestroy

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../Services/shopping-list.service';
 import { Subscription } from 'rxjs';
