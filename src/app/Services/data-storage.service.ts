4d8f0d63e5671567fe608e3bd0a8161403c0a1b9
Marina Melaré <melare.nina@gmail.com>
2020-05-04 17:27:07 -0300

    Storing Recipes

--- src/app/Services/data-storage.service.ts
+++ src/app/Services/data-storage.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from './recipe.service';
 
 @Injectable({providedIn: 'root'})
 
 export class DataStorageService{
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient,
+                private recipesService: RecipeService){}
+
+    storeRecipes(){
+        const recipes = this.recipesService.getRecipes();
+
+        // Put overwrites all the data on our server
+        this.http.put(                               // Recipes is to create a new folder, and .json is because of firebase (our backend)
+            'https://courseproject-33c56.firebaseio.com/recipes.json',
+            recipes
+        ).subscribe(response => {
+            console.log(response);
+        });
+    }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../Services/data-storage.service';
 
 @Component({
     selector: 'app-header',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 
 export class HeaderComponent{
 
+    constructor(private dsService: DataStorageService){}
+
+    onSaveData(){
+        this.dsService.storeRecipes();
+    }
 }
