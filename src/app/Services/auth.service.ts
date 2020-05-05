c3ae7d32a9202960fdbf8f7e87735bc2cade603c
Marina Melaré <melare.nina@gmail.com>
2020-05-05 13:01:34 -0300

    Adding Auto-Login

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -81,8 +81,36 @@ export class AuthService{
             expirationDate
         );
         this.user.next(user);
+        // putting the user in the local storage of the browser
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
+    autoLogin(){
+        // Gettin the user we've stored in the local storage of the browser, converted into a javascript object
+        const userData: {
+            email: string;
+            id: string,
+            _token: string;
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+
+        if (!userData){
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+
+        // Checking if it has a valid token
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+
+    }
 
     private handleError(errorResponse: HttpErrorResponse){
         let errorMessage = 'An unklnown error occurred!';
