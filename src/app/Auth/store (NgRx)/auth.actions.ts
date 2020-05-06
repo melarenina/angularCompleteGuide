840799f918bd6c7555d9d1ca3500e1764ccdb931
Marina Melaré <melare.nina@gmail.com>
2020-05-06 18:24:16 -0300

    Effects and error handling

--- src/app/Auth/store (NgRx)/auth.actions.ts
+++ src/app/Auth/store (NgRx)/auth.actions.ts
@@ -4,6 +4,12 @@ export const LOGIN = '[Auth] Login';
 export const LOGOUT = 'Auth] Logout';
 export const LOGIN_START = '[Auth] Login Start';
 
+export class LoginStart implements Action{
+    readonly type = LOGIN_START;
+
+    constructor(public payload: {email: string, password: string}){}
+}
+
 export class Login implements Action{
     readonly type = LOGIN;
 
--- src/app/Auth/store (NgRx)/auth.effects.ts
+++ src/app/Auth/store (NgRx)/auth.effects.ts
@@ -1,12 +1,44 @@
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, ofType, Effect } from '@ngrx/effects';
+import { switchMap, catchError, map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 import * as AuthActions from './auth.actions';
+import { environment } from 'src/environments/environment';
+import { of } from 'rxjs';
+
+export interface AuthResponseData{
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+}
 
 export class AuthEffects{
 
+    constructor(private actions$: Actions,
+                private http: HttpClient){}
+
+    @Effect()
     authLogin = this.actions$.pipe(
-        ofType(AuthActions.LOGIN_START) // Only continue this if the action if of type LOGIN_START
+        ofType(AuthActions.LOGIN_START), // Only continue this if the action if of type LOGIN_START
+        switchMap((authData: AuthActions.LoginStart) => { // Creates a new observable, by taking another observable's data
+                return this.http.post<AuthResponseData>(
+                'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
+                {
+                    email: authData.payload.email,
+                    password: authData.payload.password,
+                    returnSecureToken: true
+                }
+            ).pipe(catchError(error => {
+                // ...
+                of(); // returns a new observable
+            }), map(resData => {
+                of();
+            }));
+        })
     );
 
-    constructor(private actions$: Actions){}
 }
