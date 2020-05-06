d955e9244bef217357d2fcdda5b6f669c71eccc5
Marina Melaré <melare.nina@gmail.com>
2020-05-06 16:33:26 -0300

    Setting Up Auth Reducer & Actions

--- src/app/Auth/store (NgRx)/auth.reducer.ts
+++ src/app/Auth/store (NgRx)/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 export interface State{
     user: User;
@@ -8,6 +9,32 @@ const initialState: State = {
     user: null
 };
 
-export function authReducer(state: State = initialState, action){
-    return state;
+export function authReducer(state: State = initialState, action: AuthActions.AuthActions){
+    switch (action.type){
+        // ------------------------------LOGIN------------------------------
+        case AuthActions.LOGIN:
+            const newUser = new User(
+                action.payload.email,
+                action.payload.userId,
+                action.payload.token,
+                action.payload.expirationDate
+            );
+
+            return {
+                ...state,
+                user: newUser
+            };
+        // ------------------------------LOGIN------------------------------
+
+        // ------------------------------LOGOUT------------------------------
+        case AuthActions.LOGOUT:
+            return{
+                ...state,
+                user: null
+            };
+        // ------------------------------LOGOUT------------------------------
+
+        default:
+            return state;
+    }
 }
