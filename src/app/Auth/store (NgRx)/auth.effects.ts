1a8f4b2676cdbb0c5e091b88c9508ac407f991d9
Marina Melaré <melare.nina@gmail.com>
2020-05-06 19:16:04 -0300

    Finishing the Login Effect

--- src/app/Auth/store (NgRx)/auth.effects.ts
+++ src/app/Auth/store (NgRx)/auth.effects.ts
@@ -1,11 +1,12 @@
 import { Actions, ofType, Effect } from '@ngrx/effects';
-import { switchMap, catchError, map } from 'rxjs/operators';
+import { switchMap, catchError, map, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 import * as AuthActions from './auth.actions';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData{
     kind: string;
@@ -21,9 +22,10 @@ export interface AuthResponseData{
 export class AuthEffects{
 
     constructor(private actions$: Actions,
-                private http: HttpClient){}
+                private http: HttpClient,
+                private router: Router){}
 
-    @Effect()
+    @Effect() // This effect returns a new action
     authLogin = this.actions$.pipe(
         ofType(AuthActions.LOGIN_START), // Only continue this if the action if of type LOGIN_START
         switchMap((authData: AuthActions.LoginStart) => { // Creates a new observable, by taking another observable's data
@@ -36,18 +38,50 @@ export class AuthEffects{
                 }
             ).pipe(map(resData => {
                 const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-                return of(new AuthActions.Login({
+                return new AuthActions.Login({
                     email: resData.email,
                     userId: resData.localId,
                     token: resData.idToken,
                     expirationDate
-                }));
-            }), catchError(error => {
-                    // ...
-                    return of(); // returns a new observable
-                })
+                });
+            }), catchError(errorResponse => {
+
+                let errorMessage = 'An unknown error occurred!';
+
+                if (!errorResponse.error || !errorResponse.error.error){
+                    return of(new AuthActions.LoginFail(errorMessage));
+                }
+
+                switch (errorResponse.error.error.message) {
+
+                    case 'EMAIL_EXISTS':
+                        errorMessage = 'This email already exists!';
+                        break;
+
+                    case 'EMAIL_NOT_FOUND':
+                        errorMessage = 'Invalid email.';
+                        break;
+
+                    case 'INVALID_PASSWORD':
+                        errorMessage = 'Incorrect Password.';
+                        break;
+
+                }
+
+                // returns a new observable
+                return of(new AuthActions.LoginFail(errorMessage));
+            })
             );
         })
     );
+
+    @Effect({dispatch: false}) // This effect doesn't return a new action
+    authSuccess = this.actions$.pipe(
+        ofType(AuthActions.LOGIN),
+        tap(() => {
+            this.router.navigate(['/']);
+        })
+    );
+
 
 }
