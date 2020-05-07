03c1836889b9e5bd1aa4c3d4671c94e9cc6bbaef
Marina Melaré <melare.nina@gmail.com>
2020-05-06 21:13:53 -0300

    Finishing the Auth Effects

--- src/app/Auth/auth.component.ts
+++ src/app/Auth/auth.component.ts
@@ -1,12 +1,10 @@
 import { Component, ComponentFactoryResolver, ViewChild, OnDestroy, OnInit } from '@angular/core';
 import { NgForm, Form } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { AlertComponent } from '../shared/alert.component';
 import { PlaceHolderDirective } from '../shared/placeholder/placeholder.directive';
-import { AuthService, AuthResponseData } from './auth.service';
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store (NgRx)/auth.actions';
 
@@ -25,9 +23,7 @@ export class AuthComponent implements OnDestroy, OnInit{
     private storeSub: Subscription;
     @ViewChild(PlaceHolderDirective, {static: false}) alertHost: PlaceHolderDirective;
 
-    constructor(private authService: AuthService,
-                private router: Router,
-                private componentFactoryResolver: ComponentFactoryResolver,
+    constructor(private componentFactoryResolver: ComponentFactoryResolver,
                 private store: Store<fromApp.AppState>){}
                 // Component factory allows us to get access to the component factory, in which we pass the
                 // component we want to have access, so that angular creates it for us
--- src/app/Auth/auth.service.ts
+++ src/app/Auth/auth.service.ts
@@ -4,16 +4,6 @@ import * as fromApp from '../store/app.reducer';
 import * as AuthActions from './store (NgRx)/auth.actions';
 import { Store } from '@ngrx/store';
 
-export interface AuthResponseData{
-    kind: string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
-    registered?: boolean;
-}
-
 @Injectable({providedIn: 'root'})
 
 export class AuthService{
