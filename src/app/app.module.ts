973b03ad9d195bea01ba37cde8e1743b5ff18b3c
Marina Melaré <melare.nina@gmail.com>
2020-05-06 21:22:26 -0300

    Using the Store Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtools, StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,8 +11,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { StoreModule } from '@ngrx/store';
 import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './Auth/store (NgRx)/auth.effects';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { AuthEffects } from './Auth/store (NgRx)/auth.effects';
     SharedModule,
     CoreModule,
     StoreModule.forRoot(fromApp.appReducer),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     EffectsModule.forRoot([AuthEffects])
   ],
   // All the services you want to provide (or use the providedin on the injectable)
